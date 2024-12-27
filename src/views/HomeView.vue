8ee8ebbc11c179aa4a586ff8e9bdc9e328cea4ec
Soledad <soledadcot@gmail.com>
2024-12-27 11:18:44 +0100

    start project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class='title'>
+    Helene TRAN
+  </main>
+</template>
+
+<style scoped>
+.title {
+    /* font-family: 'LeagueGothicSatoshi', Arial, sans-serif; */
+  font-family:Arial, Helvetica, sans-serif;
+  color: blue;
+}
+</style>
