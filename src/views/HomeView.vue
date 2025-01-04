7177836a174d20db80226ea77fa1b90c302c13dd
antoinezl <lu.zhao.antoine@gmail.com>
2025-01-04 17:01:39 +0100

    creation du component Experience + test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import ExpericeGallery from '../components/ExperienceGallery.vue';
 </script>
 
 <template>
   <main class='title'>
     Helene TRAN
+    <ExpericeGallery expTitle="ARLES" expDates="08-2024" 
+      expDescription="Arles c'est une belle ville quand même !"
+      />
+
+    <ExpericeGallery expTitle="LOMO" expDates="08-2024" 
+    expDescription="Arles c'est une belle ville quand même !" 
+    />
   </main>
 </template>
 
